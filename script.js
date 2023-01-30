75d17e35455f0b57f9986a6cbd7d0cae9e9a516b
vlts1 <vladyslavtsepenok@gmail.com>
2023-01-29 21:44:36 -0800

    Code added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+window.onload = function(){ 
+    document.getElementById("showAll").onclick = function () {
+        var radioButtonGroup = document.getElementsByName("exercises");
+        var checkedRadio = Array.from(radioButtonGroup).find(
+          (radio) => radio.checked
+        );
+        alert("You have selected : " + checkedRadio.value);
+      };
+    };
+    
